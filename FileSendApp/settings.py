cbb5237eb5adb2e1a132fbf8921e27f04057fdb0
shi0n0 <shiono.yui1080@gmail.com>
2022-12-13 12:33:44 +0900

    データベースの更新

--- FileSendApp/settings.py
+++ FileSendApp/settings.py
@@ -77,8 +77,16 @@ WSGI_APPLICATION = 'FileSendApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'filesenddb',
+        'USER': 'filesenduser',
+        'PASSWORD': 'filesendpassword',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        # MySQLで日本語が使えるようにする設定
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
